d9e0a8e5c9170e0101aea972f76755513103d73f
Dam A <cgasconl@iesch.org>
2022-01-31 20:23:51 +0100

    bgbg

--- 1Trimestre/Proyecto_T2_Final_Mantenimeinto/src/main/java/com/mycompany/proyecto_t2_final_mantenimeinto/PantallaDeTrabajo.java
+++ 1Trimestre/Proyecto_T2_Final_Mantenimeinto/src/main/java/com/mycompany/proyecto_t2_final_mantenimeinto/PantallaDeTrabajo.java
@@ -5,18 +5,99 @@
  */
 package com.mycompany.proyecto_t2_final_mantenimeinto;
 
+import com.mycompany.proyecto_t2_final_mantenimeinto.Clases.Profesor;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author damA
  */
 public class PantallaDeTrabajo extends javax.swing.JDialog {
+    
+    Conectar conectar = null;
+    List<Profesor> listaPelis = new ArrayList<Profesor>();
 
     /**
      * Creates new form PantallaDeTrabajo
      */
-    public PantallaDeTrabajo(java.awt.Frame parent, boolean modal) {
+    public PantallaDeTrabajo(java.awt.Frame parent, boolean modal) throws SQLException {
         super(parent, modal);
         initComponents();
+        RefrescarIncidencias();
+    }
+    
+    
+    // Hacemos un Select con todas las incidencias
+    public void RefrescarIncidencias() throws SQLException{
+        DefaultTableModel dtm = new DefaultTableModel();
+        // Modelo de la tabla
+        dtm.setColumnIdentifiers(new String[]{"Id Incidencia","Creada por","Descripcion","Descripción Técnica","Horas","Estado","Lanzamiento Incidencia",
+                                              "Inicio Reparacion","Fin Reparación","Nivel","Clase","Edificio","Observaciones"});
+        
+        
+        
+        //for (Clientes cliente: listaclientes){
+        //    dtm.addRow(cliente.toArrayString());
+        //}
+        
+       
+        // Conexión
+        conectar = new Conectar();
+        Connection conexion = conectar.getConnection();
+        
+        if (conexion != null) {
+            
+            
+            Statement s = conexion.createStatement();
+            // Le pasamos la consulta
+            ResultSet rs = s.executeQuery("select i.id_incidencia, p.nombre_completo, i.descripcion, i.desc_tecnica,\n" +
+                                          " i.horas, est.estado, i.fecha,i.fecha_ini_rep, i.fecha_fin_rep, urg.urgencia, \n" +
+                                          " ubi.ubicacion, edi.edificio, i.observaciones\n" +
+                                          "from man_incidencias i\n" +
+                                          "inner join man_reparacion r on r.id_incidencia = i.id_incidencia\n" +
+                                          "inner join fp_profesor p on p.id_profesor = r.id_profesor\n" +
+                                          "inner join man_estado est on est.id_estado = i.id_estado\n" +
+                                          "inner join man_urgencia urg on urg.id_urgencia = i.nivel_urgencia\n" +
+                                          "inner join man_ubicacion ubi on ubi.id_ubicacion = i.id_ubicacion\n" +
+                                          "inner join man_edificio edi on edi.id_edificio = ubi.id_edificio ");
+                   
+            // Generamos un array para recoger lo que pedimos en la consulta
+            String incide[] = new String[13];
+                       
+           
+            while (rs.next()) {
+                incide[0] = rs.getString(1);
+                incide[1] = rs.getString(2);
+                incide[2] = rs.getString(3);
+                incide[3] = rs.getString(4);
+                incide[4] = rs.getString(5);
+                incide[5] = rs.getString(6);
+                incide[6] = rs.getString(7);
+                incide[7] = rs.getString(8);
+                incide[8] = rs.getString(9);
+                incide[9] = rs.getString(10);
+                incide[10] = rs.getString(11);
+                incide[11] = rs.getString(12);
+                incide[12] = rs.getString(13);
+                
+              
+                // Añadimos una fila a la tabla
+                dtm.addRow(incide);
+            }
+            // Le ponemos modelo a la tabla
+            tablaIncidencias.setModel(dtm);
+                                    
+        }
+        else{
+            JOptionPane.showMessageDialog(this, "conexion fallida");
+        }
     }
 
     /**
@@ -29,11 +110,17 @@ public class PantallaDeTrabajo extends javax.swing.JDialog {
     private void initComponents() {
 
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tablaIncidencias = new javax.swing.JTable();
+        btnCrearIncidencia = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tablaIncidencias.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -44,68 +131,67 @@ public class PantallaDeTrabajo extends javax.swing.JDialog {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane2.setViewportView(jTable1);
+        jScrollPane2.setViewportView(tablaIncidencias);
+
+        btnCrearIncidencia.setText("Crear Incidencia");
+        btnCrearIncidencia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCrearIncidenciaActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("Incidencias");
+
+        jMenuItem1.setText("Abiertas");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Cerradas");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 1077, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnCrearIncidencia, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 127, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 459, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(112, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 397, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnCrearIncidencia, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(11, 11, 11))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PantallaDeTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PantallaDeTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PantallaDeTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PantallaDeTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void btnCrearIncidenciaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCrearIncidenciaActionPerformed
+        
+    }//GEN-LAST:event_btnCrearIncidenciaActionPerformed
 
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                PantallaDeTrabajo dialog = new PantallaDeTrabajo(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCrearIncidencia;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable tablaIncidencias;
     // End of variables declaration//GEN-END:variables
 }
